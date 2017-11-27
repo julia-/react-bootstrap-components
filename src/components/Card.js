b57ff0e8b64c23bfb516f1c43f4fba49d5e81e3c
julia- <julia.ryanx@gmail.com>
2017-11-27 21:59:33 +1100

    create card component

---
+++ src/components/Card.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Card = ({
+  bgColor = 'light',
+  textColor = 'dark',
+  children
+}) => (
+  <div className={`card bg-${bgColor} text-${textColor}` }>
+    { children }
+  </div>
+)
+
+const CardHeader = ({
+  children
+}) => (
+  <div class="card-header">
+    { children }
+  </div>
+)
+
+const CardBody = ({
+  children
+}) => (
+  <div className="card-body">
+    { children }
+  </div>
+)
+
+const CardTitle = ({
+  children
+}) => (
+    <h4 className="card-title">
+      {children}
+    </h4>
+  )
+
+const CardText = ({
+  children
+}) => (
+  <p className="card-text">
+    {children}
+  </p>
+)
+
+export { Card, CardHeader, CardBody, CardTitle, CardText }
