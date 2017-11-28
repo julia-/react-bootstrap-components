d9d8a43857bad515731fff21f3445b42a0904333
julia- <julia.ryanx@gmail.com>
2017-11-28 11:10:55 +1100

    add container with cols and rows

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap-4.0.0-beta.2-dist/css/bootstrap.css'
 import {Button, ButtonGroup} from './components/Button'
+import Container from './components/Container'
+import Row from './components/Row'
+import Col from './components/Col'
 import Alert from './components/Alert'
 import { Nav, NavItem } from './components/Nav'
 import { Card, CardHeader, CardBody, CardTitle, CardText } from './components/Card'
@@ -13,77 +16,87 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className='mt-3'>
-          <Button title='Google' href='https://www.google.com/' />
-          <Button title='Second' variation='primary' />
-          <Button title='Third' variation='success' />
-        </div>
-        <div className='mt-3'>
-          <Nav>
-            <NavItem>First</NavItem>
-            <NavItem active={ true }>Second</NavItem>
-            <NavItem>Third</NavItem>
-          </Nav>
-        </div>
-        <Alert role='alert'>
-          This is a primary alert—check it out!
-        </Alert>
-        <Alert alertType='info'>
-          This is a info alert—check it out!
-        </Alert>
-        <Alert alertType='secondary'>
-          This is a secondary alert—check it out!
-        </Alert>
-        <Card bgColor='info' textColor='white'>
-          <CardHeader>
-            Card Header
-          </CardHeader>
-          <CardBody>
-            <CardTitle>Card Title</CardTitle>
-            <CardText>Card Text</CardText>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardBody>
-            Card Body
-          </CardBody>
-        </Card>
-        <Form action='/sign-up' method='POST'>
-          <FormGroup>
-            <FormLabel>
-              Name
-            </FormLabel>
-            <FormInput />
-          </FormGroup>
-          <FormGroup>
-            <FormLabel>
-              Email
-            </FormLabel>
-            <FormInput formType='email' sizing='lg' id='email' />
-          </FormGroup>
-        </Form>
-        <ProgressBar currentValue='25' />
-        <ProgressBar currentValue='50' bgColor='info' />
-        <ProgressBar currentValue='75' bgColor='warning' striped={ true } />
-        <ProgressBar currentValue='100' striped={ true } />
-        <ButtonGroup>
-          <Button title='1' variation='success' />
-          <Button title='2' variation='success' />
-          <Button title='3' variation='success' />
-        </ButtonGroup>
-        <ButtonGroup buttonType='toolbar'>
-          <Button title='1' variation='success' />
-          <Button title='2' variation='success' />
-          <Button title='3' variation='success' />
-        </ButtonGroup>
-        <ButtonGroup inputGroup={true}>
-          <Button title='1' variation='success' />
-          <Button title='2' variation='success' />
-          <Button title='3' variation='success' />
-        </ButtonGroup>
+        <Container>
+          <Row>
+            <Col>A</Col>
+            <Col>B</Col>
+          </Row>
+          <Row>
+            <Col width={4}>A</Col>
+            <Col width={8}>B</Col>
+          </Row>
+          <div className='mt-3'>
+            <Button title='Google' href='https://www.google.com/' />
+            <Button title='Second' variation='primary' />
+            <Button title='Third' variation='success' />
+          </div>
+          <div className='mt-3'>
+            <Nav>
+              <NavItem>First</NavItem>
+              <NavItem active={ true }>Second</NavItem>
+              <NavItem>Third</NavItem>
+            </Nav>
+          </div>
+          <Alert role='alert'>
+            This is a primary alert—check it out!
+          </Alert>
+          <Alert alertType='info'>
+            This is a info alert—check it out!
+          </Alert>
+          <Alert alertType='secondary'>
+            This is a secondary alert—check it out!
+          </Alert>
+          <Card bgColor='info' textColor='white'>
+            <CardHeader>
+              Card Header
+            </CardHeader>
+            <CardBody>
+              <CardTitle>Card Title</CardTitle>
+              <CardText>Card Text</CardText>
+            </CardBody>
+          </Card>
+          <Card>
+            <CardBody>
+              Card Body
+            </CardBody>
+          </Card>
+          <Form action='/sign-up' method='POST'>
+            <FormGroup>
+              <FormLabel>
+                Name
+              </FormLabel>
+              <FormInput />
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>
+                Email
+              </FormLabel>
+              <FormInput formType='email' sizing='lg' id='email' />
+            </FormGroup>
+          </Form>
+          <ProgressBar currentValue='25' />
+          <ProgressBar currentValue='50' bgColor='info' />
+          <ProgressBar currentValue='75' bgColor='warning' striped={ true } />
+          <ProgressBar currentValue='100' striped={ true } />
+          <ButtonGroup>
+            <Button title='1' variation='success' />
+            <Button title='2' variation='success' />
+            <Button title='3' variation='success' />
+          </ButtonGroup>
+          <ButtonGroup buttonType='toolbar'>
+            <Button title='1' variation='success' />
+            <Button title='2' variation='success' />
+            <Button title='3' variation='success' />
+          </ButtonGroup>
+          <ButtonGroup inputGroup={true}>
+            <Button title='1' variation='success' />
+            <Button title='2' variation='success' />
+            <Button title='3' variation='success' />
+          </ButtonGroup>
+        </Container>
       </div>
     );
   }
 }
 
-export default App;
+export default App
